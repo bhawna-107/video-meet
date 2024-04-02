f7e313f7a856fd04a98daf04c6d8c5fa0e25a92c
bhawna-107 <bhawnabatra107@gmail.com>
2024-04-02 11:40:10 +0530

    frontend started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Landing } from './components/Landing';
+import { Room } from './components/Room';
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Landing />} />
+      <Route path='/room' element={<Room />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
